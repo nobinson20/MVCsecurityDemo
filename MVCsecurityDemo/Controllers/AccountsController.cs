d71b519742f45c9957dcd8bef928ccf28f6b7c90
nobinson20 <nobinson20@gmail.com>
2022-04-20 01:32:25 +0900

    Add project files.

---
+++ MVCsecurityDemo/Controllers/AccountsController.cs
@@ -0,0 +1,62 @@
+﻿using MVCsecurityDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCsecurityDemo.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            using (MVC_DBEntitiesHmm context = new MVC_DBEntitiesHmm())
+            {
+                bool IsValidUser = context.Users.Any(user => user.UserName.ToLower() ==
+                     model.UserName.ToLower() && user.UserPassword == model.UserPassword);
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "invalid Username or Password");
+                return View();
+            }
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (MVC_DBEntitiesHmm context = new MVC_DBEntitiesHmm())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ MVCsecurityDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCsecurityDemo.MvcApplication" Language="C#" %>
